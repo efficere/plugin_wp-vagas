185001a16bb01d6a4de296b772023be6c5ed0a01
Michel <mfagundesleal@outlook.com>
2022-12-08 10:19:31 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/*
+    Plugin Name: Sistema de vagas e banco de talentos
+    Description: Sistema destinado a disponibilizar vagas, gerenciar currículos e armazená-los para consulta posterior caso necessário.
+    Version: 0.0.1
+    Author: Efficere Sistemas e Informática
+    Author URI: https://sisefficere.com.br
+*/
